e1464da4e31a82d04a6ece077893f0c773ecd82f
Sang <sangfc774@gmail.com>
2022-09-12 08:33:10 +0700

    java_string_boot

---
+++ java/src/main/java/h_sang/java/dtos/StudentDTO.java
@@ -0,0 +1,22 @@
+package h_sang.java.dtos;
+
+public class StudentDTO {
+    private String lastName;
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String nFirstName) {
+        firstName = nFirstName;
+    }
+
+    public void setLastName(String nLastName) {
+        lastName = nLastName;
+    }
+}
